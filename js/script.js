a808b29a3e31f60cb121804f7bd1f1d2a82cd2c8
Ilham Julianto <ilhamjulianto98@gmail.com>
2024-11-30 00:18:39 +0700

    create parallax

--- js/script.js
+++ js/script.js
@@ -17,3 +17,43 @@ $('.page-scroll').on('click', function(e){
 	e.preventDefault();
 
 });
+
+
+// Parallax
+
+// about
+$(window).on('load', function() {
+	$('.pKiri').addClass('pTampil');
+	$('.pKanan').addClass('pTampil');
+});
+
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+
+	// Jumbotron
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+wScroll/4+'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+wScroll/2+'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+wScroll/1.5+'%)'
+	});
+
+	// porfolio
+	if(wScroll > $('.portfolio').offset().top - 250) {
+		$('.portfolio .thumbnail').each(function(i){
+			setTimeout(function(){
+				$('.portfolio .thumbnail').eq(i).addClass('tampil')
+			}, 300 * (i+1));
+			
+
+		})
+		
+	}
+
+
+});
